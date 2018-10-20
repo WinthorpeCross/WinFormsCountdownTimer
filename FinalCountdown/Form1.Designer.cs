fb6dff26b52382af8ae1c4160e6b6186c872199b
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-10-20 18:41:44 +0100

    First Commit

---
+++ FinalCountdown/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FinalCountdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnExtend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(52, 43);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 44);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Trigger Call Wrap Up Time";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(230, 48);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(73, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "<<<Status>>>";
+            // 
+            // btnExtend
+            // 
+            this.btnExtend.Location = new System.Drawing.Point(223, 82);
+            this.btnExtend.Name = "btnExtend";
+            this.btnExtend.Size = new System.Drawing.Size(110, 41);
+            this.btnExtend.TabIndex = 2;
+            this.btnExtend.Text = "Extend Call Wrap Up Time";
+            this.btnExtend.UseVisualStyleBackColor = true;
+            this.btnExtend.Click += new System.EventHandler(this.btnExtend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExtend);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnExtend;
+    }
+}
+
---
+++ FinalCountdown/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FinalCountdown
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int counter = 30;
+        private int standardWrapUpTime = 30;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnExtend.Enabled = false;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+                timer.Stop();
+                btnExtend.Enabled = false;
+            }
+
+            lblStatus.Text = string.Format("You have {0} seconds call wrap-up time remaining...", counter); 
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer = new Timer() { Interval = 1000 };
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+            lblStatus.Text = string.Format("You have been allocated {0} seconds call wrap-up time...", standardWrapUpTime);
+            btnExtend.Enabled = true;
+        }
+
+        private void btnExtend_Click(object sender, EventArgs e)
+        {
+            if (counter != 0)
+            {
+                counter = counter + standardWrapUpTime;
+            }
+        }
+    }
+}
