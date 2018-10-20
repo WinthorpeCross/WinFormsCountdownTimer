0287af1492fb4a894e7f04f3a99d51d9df2da57a
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-10-20 19:04:00 +0100

    Form1 changes

--- FinalCountdown/Form1.Designer.cs
+++ FinalCountdown/Form1.Designer.cs
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 175);
             this.Controls.Add(this.btnExtend);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnStart);
